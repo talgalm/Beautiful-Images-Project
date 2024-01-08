11ec69a40258585c56b6a9aaea9c4f5c0bccbd44
Gil Khais <gil4390@gmail.com>
2024-01-08 11:57:16 +0200

    Add navigation buttons to App component

--- Frontend/beautiful-images-frontend/src/App.js
+++ Frontend/beautiful-images-frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
-
   return (
     <div className="App">
       <h1>APPPPPPP</h1>
+      <nav>
+        <Button variant="primary"><Link to="/home" className="text-light">Home</Link></Button>{' '}
+        <Button variant="primary"><Link to="/login" className="text-light">Login</Link></Button>{' '}
+        <Button variant="primary"><Link to="/images" className="text-light">Images Page</Link></Button>{' '}
+        <Button variant="primary"><Link to="/rating" className="text-light">Rating</Link></Button>{' '}
+        <Button variant="primary"><Link to="/admin" className="text-light">Admin</Link></Button>{' '}
+      </nav>
     </div>
   );
 }
